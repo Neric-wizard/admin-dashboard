3d1f02e4bcf62b29a51e8863bfbf22794ff5519e
Njeck Neric <njeckneric507@gmail.com>
2026-03-15 20:45:18 +0100

    Fix desktop layout - restore sidebar and grid

--- app/page.tsx
+++ app/page.tsx
@@ -10,43 +10,41 @@ import MobileMenu from "./components/MobileMenu";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-950 overflow-y-auto">
+    <div className="min-h-screen bg-gray-950">
       <MobileMenu />
       
-      {/* Desktop Sidebar - hidden on mobile */}
+      {/* Sidebar - visible on desktop only */}
       <div className="hidden lg:block">
         <Sidebar />
       </div>
       
-      {/* Main Content */}
-      <main className="lg:ml-64 w-full">
-        <div className="pt-16 lg:pt-6 px-3 sm:px-4 lg:px-6 pb-10">
-          <Header />
-          <QuickActions />
-          <StatsCards />
-          
-          {/* Sales Chart + Activity */}
-          <div className="flex flex-col lg:grid lg:grid-cols-3 gap-4 lg:gap-6 mt-4 lg:mt-6">
-            <div className="lg:col-span-2 w-full">
-              <SalesChart />
+      {/* Main Content - with left margin on desktop */}
+      <main className="lg:ml-64 min-h-screen">
+        <div className="pt-16 lg:pt-6 px-4 lg:px-6 pb-10">
+          {/* Desktop Layout - grid for desktop, stack for mobile */}
+          <div className="lg:block">
+            <Header />
+            <QuickActions />
+            <StatsCards />
+            
+            {/* Two column layout for desktop */}
+            <div className="lg:grid lg:grid-cols-3 lg:gap-6 mt-6">
+              <div className="lg:col-span-2">
+                <SalesChart />
+              </div>
+              <div className="lg:col-span-1">
+                <ActivityTimeline />
+              </div>
             </div>
-            <div className="lg:col-span-1 w-full">
-              <ActivityTimeline />
+            
+            <div className="mt-6">
+              <RecentOrders />
+            </div>
+            
+            <div className="mt-6">
+              <UserManagement />
             </div>
           </div>
-          
-          {/* Recent Orders */}
-          <div className="mt-4 lg:mt-6 w-full">
-            <RecentOrders />
-          </div>
-          
-          {/* User Management */}
-          <div className="mt-4 lg:mt-6 w-full">
-            <UserManagement />
-          </div>
-          
-          {/* Extra padding for scrolling */}
-          <div className="h-10"></div>
         </div>
       </main>
     </div>
